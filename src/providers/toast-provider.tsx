c91468122e312dfc02ef474f90f2c9cae89019ce
imtiaz453 <143900031+imtiaz453@users.noreply.github.com>
2026-05-31 00:35:53 +0500

    various fixes

--- src/providers/toast-provider.tsx
+++ src/providers/toast-provider.tsx
@@ -145,7 +145,7 @@ export function ToastProvider({ children }: { children: ReactNode }) {
         {
           id,
           duration: 3400,
-          position: "top-right",
+          position: "bottom-right",
         },
       );
     },
